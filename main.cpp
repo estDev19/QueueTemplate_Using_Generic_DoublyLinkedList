63afb2dfcda6ce69d309d2588a8807bac46201c9
esteban0803 <estebancastrooviedo@gmail.com>
2023-04-07 18:47:29 -0600

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "Queue.cpp"
+#include "Exception.h"
+#include "Client.h"
+int main() {
+    try {
+        Queue<Client> *queue = new Queue<Client>;
+
+        queue->enqueue(new Client("John",27));
+        queue->enqueue(new Client("Olivia",35));
+        queue->enqueue(new Client("Harry", 17));
+        queue->enqueue(new Client("Elizabeth", 40));
+        queue->enqueue(new Client("Jack",38));
+
+        cout <<"Queue size: "<< queue->size() << endl;
+        cout << "Queue values: " << endl;
+        cout << *queue->front();
+        queue->dequeue();
+        cout << *queue->front();
+        queue->dequeue();
+        cout << *queue->front();
+        queue->dequeue();
+        cout << *queue->front();
+        queue->dequeue();
+        cout << *queue->front();
+        queue->dequeue();
+        queue->dequeue();   //will generate an error (Exception class)
+        delete queue;
+    }
+    catch (Exception e){
+        cout<<endl<<"Exception: "<<e.getMessage();
+    }
+    return 0;
+}
